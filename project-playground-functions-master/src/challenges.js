86e0556df7b9a6c236c73d276ed0ec4fcabf5baa
Carlos Henrique <henrique.joaovictor@hotmail.com>
2022-05-05 19:59:24 -0300

    add a new feacture

--- project-playground-functions-master/src/challenges.js
+++ project-playground-functions-master/src/challenges.js
@@ -1,49 +1,57 @@
 // Desafio 1
+
 function compareTrue(n1, n2, n3 = 5) {
   if (n1 > n3 && n2 > n3) {
     return true;
   }
   return false;
 }
-console.log(compareTrue(5, 5));
 
 // Desafio 2
+
 function calcArea(base, height) {
   const triangle = (base * height) / 2;
   return triangle;
 }
 
 // Desafio 3
+
 function splitSentence(phrase) {
   const wordWithSplit = phrase.split(' ');
   return wordWithSplit;
 }
 
 // Desafio 4
+
 function concatName(arrayOfString) {
-  // const lastPosition = arrayOfString[arrayOfString.length - 1];
-  // const firstPosition = arrayOfString[0];
-  // const nameConcat = `${lastPosition}, ${firstPosition} `;
-  // return nameConcat;
+// arrayOfString.splice(1, 1);
+// arrayOfString.splice(1, 2);
+// arrayOfString.reverse();
+// return arrayOfString;
+// const lastPosition = arrayOfString[arrayOfString.length - 1];
+// const firstPosition = arrayOfString[0];
+// const nameConcat = `${lastPosition}, ${firstPosition} `;
+// return nameConcat;
 
   // const first = [...arrayOfString].shift();
   // const last = [...arrayOfString].pop();
   // return `${last}, ${first}`;
 
-  const { length, 0: first, [length - 1]: last } = arrayOfString;
-  return `${last}, ${first}`;
+  // const { length, 0: first, [length - 1]: last } = arrayOfString;
+  // return `${last}, ${first}`;
+  return arrayOfString;
 }
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  // const pointWin = wins * 3;
-  // const pointTie = ties * 1;
-  // const points = pointWin + pointTie;
-  // return points;
-  const points = (wins * 3) + ties;
+  const pointWin = wins * 3;
+  const pointTie = ties * 1;
+  const points = pointWin + pointTie;
   return points;
+  // const points = (wins * 3) + ties;
+  // return points;
 }
-
+console.log(footballPoints(0, 0));
 // Desafio 6
 function highestCount() {
   // seu código aqui
